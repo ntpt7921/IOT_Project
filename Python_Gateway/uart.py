e29ef0a40cf1f7a8d0ed61be25c7347e3ca9404d
dongghoul <78191012+dongghoul@users.noreply.github.com>
2023-04-23 19:15:09 +0700

    error control for uart.py

--- Python_Gateway/uart.py
+++ Python_Gateway/uart.py
@@ -1,49 +1,68 @@
 import serial.tools.list_ports
 
 def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "CP210x" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    # return commPort
-    return "COM5"
+    try:
+        ports = serial.tools.list_ports.comports()
+        N = len(ports)
+        commPort = "None"
+        for i in range(0, N):
+            port = ports[i]
+            strPort = str(port)
+            if "CP210x" in strPort:
+                splitPort = strPort.split(" ")
+                commPort = (splitPort[0])
+        return commPort
+        # return "COM5"
+    except Exception as e:
+        print("Error:", e)
+        return None
 
-if getPort() != "None":
-    ser = serial.Serial(port=getPort(), baudrate=115200)
-    print(ser)
+try:
+    port = getPort()
+    if port is None:
+        raise Exception("No valid serial port selected.")
+    ser = serial.Serial(port=port, baudrate=115200)
+    print("Serial PORT: " + str(ser))
+except Exception as e:
+    print("Error:", e)
+
 
 mess = ""
 def processData(client, data):
-    data = data.replace("!", "")
-    data = data.replace("#", "")
-    splitData = data.split(":")
-    print(splitData)
-    if splitData[1] == "T":
-        client.publish("cambien1", splitData[2])
-    elif splitData[1] == "H":
-        client.publish("cambien2", splitData[2])
-    elif splitData[1] == "L":
-        client.publish("cambien3", splitData[2])
+    try:
+        data = data.replace("!", "")
+        data = data.replace("#", "")
+        splitData = data.split(":")
+        print(splitData)
+        if splitData[1] == "T":
+            client.publish("cambien1", splitData[2])
+        elif splitData[1] == "H":
+            client.publish("cambien2", splitData[2])
+        elif splitData[1] == "L":
+            client.publish("cambien3", splitData[2])
+    except Exception as e:
+        print("Error:", e)
 
 mess = ""
 def readSerial(client):
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(client, mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
+    try:
+        bytesToRead = ser.inWaiting()
+        if (bytesToRead > 0):
+            global mess
+            mess = mess + ser.read(bytesToRead).decode("UTF-8")
+            while ("#" in mess) and ("!" in mess):
+                start = mess.find("!")
+                end = mess.find("#")
+                processData(client, mess[start:end + 1])
+                if (end == len(mess)):
+                    mess = ""
+                else:
+                    mess = mess[end+1:]
+    except Exception as e:
+        print("Error:", e)
 
 def writeData(data):
-    ser.write(str(data).encode())
+    try:
+        ser.write(str(data).encode())
+    except Exception as e:
+        print("Error:", e)
